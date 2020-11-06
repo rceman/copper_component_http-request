0d441eee123b9b7b9aff38cbefe3b0eb62496ff0
rceman <rceman.pentest@gmail.com>
2020-11-06 17:55:10 +0200

    initial commit

---
+++ Entity/ResponseStatus.php
@@ -0,0 +1,156 @@
+<?php
+
+
+namespace App\Component\HttpRequest\Entity;
+
+class ResponseStatus
+{
+    /** @var int */
+    public $code;
+    /** @var string */
+    public $text;
+
+    const CODE_500 = 500;
+    const CODE_409 = 409;
+    const CODE_404 = 404;
+    const CODE_403 = 403;
+    const CODE_401 = 401;
+    const CODE_400 = 400;
+    const CODE_304 = 304;
+    const CODE_302 = 302;
+    const CODE_301 = 301;
+    const CODE_204 = 204;
+    const CODE_201 = 201;
+    const CODE_200 = 200;
+    const CODE_0 = 0;
+
+    const CODE_TEXT = [
+        self::CODE_500 => 'Internal Server Error',
+        self::CODE_409 => 'Conflict',
+        self::CODE_404 => 'Not Found',
+        self::CODE_403 => 'Forbidden',
+        self::CODE_401 => 'Unauthorized',
+        self::CODE_400 => 'Bad Request',
+        self::CODE_304 => 'Not Modified',
+        self::CODE_302 => 'Found',
+        self::CODE_301 => 'Moved Permanently',
+        self::CODE_204 => 'No Content',
+        self::CODE_201 => 'Created',
+        self::CODE_200 => 'OK',
+        self::CODE_0 => 'Unknown Code'
+    ];
+
+    /**
+     * ResponseStatus constructor
+     *
+     * @param int $code
+     * @param string $text
+     */
+    public function __construct($code, $text)
+    {
+        $this->code = $code;
+        $this->text = $text;
+    }
+
+    public function toString()
+    {
+        return "$this->code $this->text";
+    }
+
+    /** @return ResponseStatus */
+    public static function internalServerError()
+    {
+        return self::findByCode(self::CODE_500);
+    }
+
+    /** @return ResponseStatus */
+    public static function conflict()
+    {
+        return self::findByCode(self::CODE_409);
+    }
+
+    /** @return ResponseStatus */
+    public static function notFound()
+    {
+        return self::findByCode(self::CODE_404);
+    }
+
+    /** @return ResponseStatus */
+    public static function forbidden()
+    {
+        return self::findByCode(self::CODE_403);
+    }
+
+    /** @return ResponseStatus */
+    public static function unauthorized()
+    {
+        return self::findByCode(self::CODE_401);
+    }
+
+    /** @return ResponseStatus */
+    public static function badRequest()
+    {
+        return self::findByCode(self::CODE_400);
+    }
+
+    /** @return ResponseStatus */
+    public static function notModified()
+    {
+        return self::findByCode(self::CODE_304);
+    }
+
+    /** @return ResponseStatus */
+    public static function found()
+    {
+        return self::findByCode(self::CODE_302);
+    }
+
+    /** @return ResponseStatus */
+    public static function movedPermanently()
+    {
+        return self::findByCode(self::CODE_301);
+    }
+
+    /** @return ResponseStatus */
+    public static function noContent()
+    {
+        return self::findByCode(self::CODE_204);
+    }
+
+    /** @return ResponseStatus */
+    public static function created()
+    {
+        return self::findByCode(self::CODE_201);
+    }
+
+    /** @return ResponseStatus */
+    public static function ok()
+    {
+        return self::findByCode(self::CODE_200);
+    }
+
+    /**
+     * @param int $code
+     *
+     * @return bool
+     */
+    public static function codeExists($code)
+    {
+        return (array_key_exists($code, self::CODE_TEXT));
+    }
+
+    /**
+     * @param int $code
+     *
+     * @return ResponseStatus
+     */
+    public static function findByCode($code)
+    {
+        $code = intval($code);
+
+        if (self::codeExists($code))
+            return new self($code, self::CODE_TEXT[$code]);
+
+        return new self(self::CODE_0, self::CODE_TEXT[self::CODE_0]);
+    }
+}
---
+++ HttpRequestService.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace App\Component\HttpRequest;
+
+use App\Component\HttpRequest\Entity\ResponseStatus;
+use GuzzleHttp;
+use GuzzleHttp\Exception\BadResponseException;
+use GuzzleHttp\Exception\GuzzleException;
+
+class HttpRequestService
+{
+    private $default_request_headers = [
+        'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36'
+    ];
+
+    private $allow_redirects = true;
+    private $verify_ssl = false;
+    private $retryMaxCount = 1;
+    private $retryMsgNeedle = false;
+
+    private $default_response = [
+        "msg" => "",
+        "status" => false,
+        "result" => [],
+        "config" => [],
+        "request" => [
+            "url" => "",
+            "method" => "",
+            "body" => "",
+            "headers" => []
+        ]
+    ];
+
+    public function __construct($verify_ssl = false, $allow_redirects = true, $default_request_headers = [])
+    {
+        $this->verify_ssl = $verify_ssl;
+        $this->allow_redirects = $allow_redirects;
+        $this->default_request_headers = array_merge($this->default_request_headers, $default_request_headers);
+    }
+
+    public function setRetryMaxCount($maxCount)
+    {
+        $this->retryMaxCount = $maxCount;
+    }
+
+    public function setRetryMsgNeedle($needle)
+    {
+        $this->retryMsgNeedle = $needle;
+    }
+
+    public function GET($url, $headers = [])
+    {
+        return $this->makeRequest($url, 'get', '', $headers);
+    }
+
+    public function POST($url, $body = '', $headers = [])
+    {
+        $headers = array_merge(['Content-Type' => 'application/x-www-form-urlencoded'], $headers);
+
+        return $this->makeRequest($url, 'post', $body, $headers);
+    }
+
+    public function PUT($url, $body = '', $headers = [])
+    {
+        return $this->makeRequest($url, 'put', $body, $headers);
+    }
+
+    public function PATCH($url, $body = '', $headers = [])
+    {
+        return $this->makeRequest($url, 'patch', $body, $headers);
+    }
+
+    public function DELETE($url, $headers = [])
+    {
+        return $this->makeRequest($url, 'delete', '', $headers);
+    }
+
+    public function allowRedirects($bool = true)
+    {
+        $this->allow_redirects = $bool;
+    }
+
+    private function autoDetectContentType($method, $body, &$headers)
+    {
+        if (strtoupper($method) === 'GET')
+            return;
+
+        $decoded_body = json_decode($body);
+
+        if ($decoded_body === null && json_last_error() !== JSON_ERROR_NONE) {
+            $contentType = 'application/x-www-form-urlencoded';
+        } else {
+            $contentType = 'application/json';
+        }
+
+        $headers = array_merge(['Content-Type' => $contentType], $headers);
+    }
+
+    private function makeRetriedRequest($retryMaxCount, $url, $method, $body = '', $headers = [], $retryCount = 0)
+    {
+        $response = array_merge([], $this->default_response);
+
+        $headers = array_merge($this->default_request_headers, is_array($headers) ? $headers : []);
+
+        $this->autoDetectContentType($method, $body, $headers);
+
+        $client = new GuzzleHttp\Client(array(
+            'verify' => $this->verify_ssl
+        ));
+
+        $response["request"] = [
+            "url" => $url,
+            "method" => $method,
+            "body" => $body,
+            "headers" => $headers
+        ];
+
+        $response["config"] = [
+            "default_request_headers" => $this->default_request_headers,
+            "allow_redirects" => $this->allow_redirects,
+            "verify_ssl" => $this->verify_ssl
+        ];
+
+        if ($url === '') {
+            $response["msg"] = "(url) param is empty " . $url;
+            return $response;
+        }
+
+        try {
+            $requestResponse = $client->request($method, $url, [
+                'headers' => $headers,
+                'body' => $body,
+                'allow_redirects' => $this->allow_redirects
+            ]);
+
+            $response["status"] = true;
+        } catch (BadResponseException $e) {
+            $requestResponse = $e->getResponse();
+
+            $response["status"] = false;
+            $response["msg"] = $e->getMessage();
+        } catch (GuzzleException $e) {
+            $requestResponse = null;
+
+            $response["result"] = [
+                "status_code" => ResponseStatus::CODE_0,
+                "status_text" => ResponseStatus::CODE_TEXT[ResponseStatus::CODE_0],
+                "protocol_version" => "1.1",
+                "body" => "GuzzleException " . $e->getMessage(),
+                "body_size" => 0,
+                "headers" => []
+            ];
+
+            $response["status"] = false;
+            $response["msg"] = $e->getMessage();
+        }
+
+        if ($requestResponse !== null)
+            $response["result"] = [
+                "status_code" => $requestResponse->getStatusCode(),
+                "status_text" => $requestResponse->getReasonPhrase(),
+                "protocol_version" => $requestResponse->getProtocolVersion(),
+                "body" => $requestResponse->getBody()->getContents(),
+                "body_size" => $requestResponse->getBody()->getSize(),
+                "headers" => $requestResponse->getHeaders()
+            ];
+
+        $response["result"]["headers"]["WH-RETRY-COUNT"] = trim($retryCount);
+
+        if (array_key_exists('Content-Length', $response["result"]["headers"]))
+            $response["result"]["body_size"] = $response["result"]["headers"]['Content-Length'][0];
+
+        if (array_key_exists('x-encoded-content-length', $response["result"]["headers"]))
+            $response["result"]["body_size"] = $response["result"]["headers"]['x-encoded-content-length'][0];
+
+        if ($response["result"]["body_size"] === 0)
+            $response["result"]["body_size"] = strlen($response["result"]["body"]);
+
+        if ($this->retryMsgNeedle !== false && strpos($response["msg"], $this->retryMsgNeedle) !== false && $retryCount < $retryMaxCount)
+            $response = $this->makeRetriedRequest($retryMaxCount, $url, $method, $body, $headers, ++$retryCount);
+
+        return $response;
+    }
+
+    public function makeRequest($url, $method, $body = '', $headers = [])
+    {
+        return $this->makeRetriedRequest($this->retryMaxCount, $url, $method, $body, $headers);
+    }
+}
